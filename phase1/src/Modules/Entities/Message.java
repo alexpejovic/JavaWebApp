8bf73541ee78635478b01d24ba443b7e82f85874
Khachig Ainteblian <ainteblian99@gmail.com>
2020-10-29 13:01:34 -0400

    implemented Message entity

--- phase1/src/Modules/Entities/Message.java
+++ phase1/src/Modules/Entities/Message.java
@@ -1,4 +1,45 @@
 package Modules.Entities;
 
+import java.time.LocalDateTime;
+
 public class Message {
+    // Message content
+    private final String content;
+
+    // Sender and receiver
+    private final String sender;
+    private final String receiver;
+
+    // Date and time of message (when it was first sent)
+    private final LocalDateTime dateTime;
+
+    public Message(String content, String sender, String receiver, LocalDateTime dateTime) {
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.dateTime = dateTime;
+    }
+
+    public Message(String content, String sender, String receiver) {
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+        dateTime = LocalDateTime.now();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
 }
