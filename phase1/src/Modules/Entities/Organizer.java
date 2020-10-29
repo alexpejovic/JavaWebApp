00465fb48f4c65c665fb9edf1e835612e1b92a48
Evan Rehbeli <evan.rehbeli@mail.utoronto.ca>
2020-10-29 15:37:46 -0400

    Implemented Organizer without correct constructor call

--- phase1/src/Modules/Entities/Organizer.java
+++ phase1/src/Modules/Entities/Organizer.java
@@ -1,4 +1,23 @@
 package Modules.Entities;
 
-public class Organizer extends Attendee {
+import java.util.ArrayList;
+
+public class Organizer extends User {
+    //Store list of ids for managed Events
+    private ArrayList<String> managedEvts;
+
+    public Organizer(String username, String pwd, ArrayList<String> evtIDs, ArrayList<String> managedEvtIDs) {
+        User(username, pwd, evtIDs);
+        this.managedEvts = managedEvtIDs;
+    }
+
+    //managedEvts getter
+    public ArrayList<String> getManagedEvts() {
+        return this.managedEvts;
+    }
+
+    //manangedEvts setter
+    public void setManagedEvts(ArrayList<String> newManagedEvts) {
+        this.managedEvts = newManagedEvts;
+    }
 }
