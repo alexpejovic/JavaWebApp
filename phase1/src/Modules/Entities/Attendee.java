9e16b3064a98ccecf6282fc1cfbc87b32d96cb10
Danielle Porto <daniellefporto@gmail.com>
2020-10-29 17:09:47 -0400

    implemented Attendee entity

--- phase1/src/Modules/Entities/Attendee.java
+++ phase1/src/Modules/Entities/Attendee.java
@@ -1,4 +1,34 @@
 package Modules.Entities;
 
+import java.util.ArrayList;
+
 public class Attendee extends User {
+    // ids of events Attendee is attending
+    private ArrayList<String> eventsList;
+    private boolean noEvents;
+
+    public Attendee(String username, String password){
+        super(username, password);
+        this.eventsList = new ArrayList<>();
+        noEvents = true;
+    }
+
+    public void addEvent(String id){
+        eventsList.add(id);
+        noEvents = false;
+    }
+    public void removeEvent(String id){
+        eventsList.remove(id);
+        if (eventsList.size() == 0){
+            noEvents = true;
+        }
+    }
+
+    public ArrayList<String> getEventsList() {
+        ArrayList<String> copy = new ArrayList<>(eventsList.size());
+        for(int i = 0; i < eventsList.size(); i++){
+            copy.set(i, eventsList.get(i));
+        }
+        return copy;
+    }
 }
