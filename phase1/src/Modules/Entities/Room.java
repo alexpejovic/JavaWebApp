6ddb96e260f643400041fb0d53a57299407182d3
kate <siyuanliu3@gmail.com>
2020-10-29 17:40:20 -0600

    added some methods to Room entity

--- phase1/src/Modules/Entities/Room.java
+++ phase1/src/Modules/Entities/Room.java
@@ -25,6 +25,39 @@ public class Room {
         events.add(eventId);
     }
 
+    public void removeEvent(String eventId){
+        try{
+            events.remove(eventId);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("There is no event with that id occurring in this Room");
+        }
+    }
+
+    public ArrayList getEvents(){
+        ArrayList<String> copy = new ArrayList<>();
+
+        for (String eventId: events){
+            copy.add(eventId);
+        }
+
+        return copy;
+    }
+
+    /**
+     * Checks if a event is occurring in this room.
+     * @param eventId the id of the event that we want to check
+     * @return true iff the event is in the list of events occurring in this room
+     */
+    public boolean isEventInRoom(String eventId){
+        for (String eventsId: events){
+            if(eventId.equals(eventsId)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int getCapacity() {
         return capacity;
     }
