daf2f7751bb4f45826cc2e03a7ed26ee73d5de54
Evan Rehbeli <evan.rehbeli@mail.utoronto.ca>
2020-10-30 11:02:16 -0400

    Organizer now extends Attendee Added Javadocs

--- phase1/src/Modules/Entities/Organizer.java
+++ phase1/src/Modules/Entities/Organizer.java
@@ -2,19 +2,29 @@ package Modules.Entities;
 
 import java.util.ArrayList;
 
-//TODO: change eventIDs from <String> to <UUID>
-public class Organizer extends User {
-    //Store list of ids for managed Events
+/**
+ * Represents an Organizer who can use this program
+ * Organizers can attend and manage events
+ */
+public class Organizer extends Attendee {
+    /**
+     * An ArrayList of Event IDs that this Organizer manages
+     */
     private ArrayList<String> managedEvents;
 
+    /**
+     * @param username the username for this Organizer
+     * @param pwd the password for this Organizer
+     */
     public Organizer(String username, String pwd) {
         super(username, pwd);
         this.managedEvents = new ArrayList<>();
     }
 
-
-
-    //managedEvents getter
+    //getters
+    /**
+     * @return a soft copy of the Events this Organizer is managing
+     */
     public ArrayList<String> getManagedEvents() {
         ArrayList<String> copy = new ArrayList<>();
 
@@ -26,14 +36,23 @@ public class Organizer extends User {
     }
 
     //setters
+    /**
+     * @param eventID the ID of the new Event this Organizer will manage
+     */
     public void addManagedEvent(String eventID) {
         this.managedEvents.add(eventID);
     }
 
+    /**
+     * @param eventID the ID of the Event this Organizer is no longer manage
+     */
     public void removeManagedEvent(String eventID) {
         this.managedEvents.remove(eventID);
     }
 
+    /**
+     * @return true if and only if this Organizer is not managing any Events
+     */
     public boolean managedEventsIsEmpty() {
         int size = this.managedEvents.size();
         return size == 0;
