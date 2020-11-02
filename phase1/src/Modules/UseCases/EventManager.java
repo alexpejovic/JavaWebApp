19d4e0ec46aebd4561628572f1084b51928591a8
Alicia Rehbeli <alicia.rehbeli@mail.utoronto.ca>
2020-11-02 16:39:12 -0500

    Implemented EventManager

--- phase1/src/Modules/UseCases/EventManager.java
+++ phase1/src/Modules/UseCases/EventManager.java
@@ -1,4 +1,51 @@
 package Modules.UseCases;
 
+import Modules.Entities.Event;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class EventManager {
+
+    /**
+     * An ArrayList of all events
+     */
+    private ArrayList<Event> eventList;
+
+    public EventManager() {
+        this.eventList = new ArrayList<>();
+    }
+
+    /**
+     * Checks if the schedule is free from time to time + 1 hour. This considers all events, regardless of room number
+     *
+     * @param time Time of day
+     * @return Returns true if there are no events already occurring at time to time + 1 hour, false otherwise
+     */
+    public boolean isScheduleFree(LocalDateTime time) {
+
+        for (Event event : this.eventList) {
+            // Iterates through each event and checks if schedule is free from time to time + 1 hour
+            // (assumes 1 hour length)
+            if (!(time.until(event.getStartTime(), java.time.temporal.ChronoUnit.HOURS) > 1 ||
+                    time.until(event.getStartTime(), java.time.temporal.ChronoUnit.HOURS) < -1)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    /**
+     * Creates an event (if possible) and adds it to ArrayList events
+     *
+     * @param roomNumber the room number of the room where the event is set to take place
+     * @param startTime the time that the event starts at
+     * @return Returns true if the event was successfully created and added to events, false otherwise
+     */
+    public boolean createEvent(int roomNumber, LocalDateTime startTime) {
+        return this.eventList.add(new Event(roomNumber, startTime));
+    }
+
 }
