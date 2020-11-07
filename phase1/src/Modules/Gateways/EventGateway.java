8e5136d28570e0d75abc7077c2f49129ea84469a
Evan Rehbeli <evan.rehbeli@mail.utoronto.ca>
2020-11-06 22:11:02 -0500

    Split ReaderWriter gateway class into 3 gateway classes: UserGateway, EventGateway, and MessageGateway for better extensibility. Fixed casting when reading in stored objects so that they are instantiated as their respective classes.

---
+++ phase1/src/Modules/Gateways/EventGateway.java
@@ -0,0 +1,57 @@
+package Modules.Gateways;
+
+import Modules.Entities.Event;
+import java.util.ArrayList;
+import java.io.*;
+
+/**
+ * reads and writes files for Events
+ */
+public class EventGateway {
+
+    /**
+     * @param filename the name of the file being read
+     */
+    public void readSerFile(String filename) {
+
+        ArrayList<Event> events = null;
+        try {
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream storedEvents = new ObjectInputStream(file);
+
+            events = (ArrayList<Event>) storedEvents.readObject();
+
+            storedEvents.close();
+            file.close();
+
+            System.out.println("Successfully read Events");
+
+        } catch (FileNotFoundException e) {
+            System.out.println(filename + " is missing");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param filename the name of the file being written to
+     * @param writeEvents ArrayList of objects being written to filename
+     * @throws IOException Exception thrown when Object cannot be found
+     */
+    public void writeSerFile(String filename, ArrayList<Event> writeEvents) throws IOException {
+        try{
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream writer = new ObjectOutputStream(file);
+
+            writer.writeObject(writeEvents);
+
+            writer.close();
+            file.close();
+
+            System.out.println("successfully stored Events");
+
+        } catch (FileNotFoundException e) {
+            System.out.println(filename + " not found");
+        }
+    }
+}
--- phase1/src/Modules/Gateways/UserGateway.java
+++ phase1/src/Modules/Gateways/UserGateway.java
@@ -1,51 +1,58 @@
 package Modules.Gateways;
 
+import Modules.Entities.User;
 import java.util.ArrayList;
 import java.io.*;
 
 /**
- * reads and writes files for our program
+ * reads and writes files for Users
  */
-public class ReaderWriter {
-
+public class UserGateway {
     /**
      * @param filename the name of the file being read
      */
-    public void readSerFile(String filename) {
+    public ArrayList<User> readSerFile(String filename) {
 
-        ArrayList<Object> objects = null;
+        ArrayList<User> users = null;
         try {
             FileInputStream file = new FileInputStream(filename);
-            ObjectInputStream object = new ObjectInputStream(file);
+            ObjectInputStream storedUsers = new ObjectInputStream(file);
 
-            objects = (ArrayList<Object>) object.readObject();
+            users = (ArrayList<User>) storedUsers.readObject();
 
-            object.close();
+            storedUsers.close();
             file.close();
 
-            System.out.println("Successfully read Events");
+            System.out.println("successfully read Users");
+
+            return users;
 
         } catch (FileNotFoundException e) {
             System.out.println(filename + " is missing");
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        return users;
     }
 
     /**
      * @param filename the name of the file being written to
-     * @param writeObjects ArrayList of objects being written to filename
+     * @param writeUsers ArrayList of Users being written to filename
      * @throws IOException Exception thrown when Object cannot be found
      */
-    public void writeSerFile(String filename, ArrayList<Object> writeObjects) throws IOException {
+    public void writeSerFile(String filename, ArrayList<User> writeUsers) throws IOException {
         try{
             FileOutputStream file = new FileOutputStream(filename);
             ObjectOutputStream writer = new ObjectOutputStream(file);
 
-            writer.writeObject(writeObjects);
+            writer.writeObject(writeUsers);
 
             writer.close();
             file.close();
+
+            System.out.println("successfully stored Users");
+
         } catch (FileNotFoundException e) {
             System.out.println(filename + " not found");
         }
