8a38b1e7a2b70dd0ba0c2a53ccfbb0879c125954
Khachig Ainteblian <ainteblian99@gmail.com>
2020-11-10 03:41:24 -0500

    implemented MessageManager use case

--- phase1/src/Modules/UseCases/MessageManager.java
+++ phase1/src/Modules/UseCases/MessageManager.java
@@ -1,4 +1,85 @@
 package Modules.UseCases;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import Modules.Entities.Message;
+
 public class MessageManager {
+    // Hashmap to track messages each user has sent or received
+    private HashMap<String, ArrayList<Message>> messages = new HashMap<>();
+
+    /**
+     * Initialize a MessageManager object given an ArrayList of Message objects
+     * @param messages ArrayList of Message objects
+     */
+    public MessageManager(ArrayList<Message> messages) {
+        for (Message message : messages) {
+            // Get sender and receiver of each message
+            String[] keys = {message.getSenderID(), message.getReceiverID()};
+
+            // For each message, check if sender and receiver are already in the hashmap
+            // If not, add a new key with an arraylist containing the current message
+            // If yes, append the current message to the user's arraylist
+            for (String key : keys) {
+                if (!this.messages.containsKey(key)) {
+                    ArrayList<Message> newMessagesArray = new ArrayList<>();
+                    newMessagesArray.add(message);
+                    this.messages.put(key, newMessagesArray);
+                } else {
+                    this.messages.get(key).add(message);
+                }
+            }
+        }
+    }
+
+    /**
+     * Return an ArrayList of messages sent or received by the user with the given ID
+     * @param user The ID string of the user
+     * @return A list of messages sent or received by the user
+     */
+    public ArrayList<Message> getUserMessages(String user) {
+        ArrayList<Message> userMessages = (ArrayList<Message>) messages.get(user).clone();
+
+        Collections.sort(userMessages);
+
+        return userMessages;
+    }
+
+    /**
+     * Create a new Message object and add it to messages hashmap for corresponding users
+     * @param senderID The ID string of the sender
+     * @param receiverID The ID string of the receiver
+     * @param message The string of the message
+     */
+    public void sendMessage(String senderID, String receiverID, String message) {
+        Message newMessage = new Message(message, senderID, receiverID);
+        // Get userMessages for sender and add new message
+        messages.get(senderID).add(newMessage);
+        // Get userMessages for receiver and add new message
+        messages.get(receiverID).add(newMessage);
+    }
+
+    /**
+     * Return an ArrayList of messages between two users sorted by date
+     * @param user1 The ID string of the first user
+     * @param user2 The ID string of the second user
+     * @return An ArrayList of messages between two users sorted by date
+     */
+    public ArrayList<Message> getConversation(String user1, String user2) {
+        ArrayList<Message> conversation = new ArrayList<>();
+        for (Message message : messages.get(user1)) {
+            if (message.getReceiverID().equals(user2)) {
+                conversation.add(message);
+            }
+        }
+        for (Message message : messages.get(user1)) {
+            if (message.getSenderID().equals(user2)) {
+                conversation.add(message);
+            }
+        }
+
+        Collections.sort(conversation);
+        return conversation;
+    }
 }
--- phase1/tests/Modules/UseCases/MessageManagerTest.java
+++ phase1/tests/Modules/UseCases/MessageManagerTest.java
@@ -1,7 +1,91 @@
 package Modules.UseCases;
 
+import Modules.Entities.Message;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
-public class EventManagerTestTest {
+public class MessageManagerTest {
+    LocalDateTime message1Date = LocalDateTime.of(2020, 10, 15, 13, 25);
+    LocalDateTime message2Date = LocalDateTime.of(2020, 10, 15, 13, 26);
+    LocalDateTime message3Date = LocalDateTime.of(2020, 10, 15, 13, 35);
+    String message1ID = String.format("%s,%s,%s", "user1", "user2", message1Date);
+    String message2ID = String.format("%s,%s,%s", "user2", "user3", message2Date);
+    String message3ID = String.format("%s,%s,%s", "user3", "user1", message3Date);
+    Message message1 = new Message("yo event 2 is lit!", "user1", "user2", message1ID, message1Date);
+    Message message2 = new Message("where is the bathroom?", "user2", "user3", message2ID, message2Date);
+    Message message3 = new Message("how you doin?", "user3", "user1", message3ID, message3Date);
+
+    // Test MessageManager Constructor
+    @Test
+    public void testMessageManager() {
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(message1);
+        messages.add(message2);
+        messages.add(message3);
+        MessageManager manager = new MessageManager(messages);
+    }
+
+    // Test getUserMessages
+    @Test
+    public void testGetUserMessages() {
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(message1);
+        messages.add(message2);
+        messages.add(message3);
+        MessageManager manager = new MessageManager(messages);
+        ArrayList<Message> userMessages = manager.getUserMessages("user1");
+        assertEquals(message1, userMessages.get(0));
+        assertEquals(message3, userMessages.get(1));
+    }
+
+    // Test sendMessage
+    @Test
+    public void testSendMessage() {
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(message1);
+        messages.add(message2);
+        messages.add(message3);
+        MessageManager manager = new MessageManager(messages);
+
+        ArrayList<Message> user1Messages = manager.getUserMessages("user1");
+        ArrayList<Message> user2Messages = manager.getUserMessages("user2");
+
+        assertEquals(2, user1Messages.size());
+        assertEquals(2, user2Messages.size());
+        manager.sendMessage("user1", "user2", "never gonna give you up");
+
+        user1Messages = manager.getUserMessages("user1");
+        user2Messages = manager.getUserMessages("user2");
+        assertEquals(3, user1Messages.size());
+        assertEquals(3, user2Messages.size());
+
+        assertEquals(user1Messages.get(2), user2Messages.get(2));
+    }
+
+    // Test getConversation
+    @Test
+    public void testGetConversation() {
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(message1);
+        messages.add(message2);
+        messages.add(message3);
+        MessageManager manager = new MessageManager(messages);
+        manager.sendMessage("user1", "user2", "never gonna give you up");
+        manager.sendMessage("user2", "user1", "never gonna let you down");
+
+        ArrayList<Message> conversation = manager.getConversation("user1", "user2");
+
+        assertEquals("user1", conversation.get(0).getSenderID());
+        assertEquals("user1", conversation.get(1).getSenderID());
+        assertEquals("user2", conversation.get(2).getSenderID());
+
+        assertEquals("yo event 2 is lit!", conversation.get(0).getContent());
+        assertEquals("never gonna give you up", conversation.get(1).getContent());
+        assertEquals("never gonna let you down", conversation.get(2).getContent());
+    }
 
 }
