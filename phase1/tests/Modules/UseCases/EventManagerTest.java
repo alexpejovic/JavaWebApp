e24c3f0d4d82e0c80ffef381488a5e66b9440758
Alicia Rehbeli <alicia.rehbeli@mail.utoronto.ca>
2020-11-10 17:38:26 -0500

    Implemented multiple new methods in EventManager, wrote new tests

--- phase1/tests/Modules/UseCases/EventManagerTest.java
+++ phase1/tests/Modules/UseCases/EventManagerTest.java
@@ -27,11 +27,32 @@ public class EventManagerTest {
         assertTrue(manager.createEvent("2", eventDate1, eventDate1.plusHours(1), "event2"));
         assertTrue(manager.createEvent("1", eventDate2, eventDate2.plusHours(1), "event3"));
         assertTrue(manager.createEvent("3", eventDate3, eventDate3.plusHours(1), "event4"));
+
+        manager.renameEvent("event2", "How 2 Do Good at Something");
+        assertEquals("How 2 Do Good at Something", manager.getName("event2"));
+        assertNotEquals("How (not) 2 Do Good at Something", manager.getName("event2"));
     }
 
     @Test
-    public void testGetters() {
+    public void testCanBook() {
+        EventManager manager = new EventManager();
+        LocalDateTime eventDate1 = LocalDateTime.of(2020, 11, 3, 11, 11);
+        LocalDateTime eventDate2 = LocalDateTime.of(2020, 12, 3, 10, 11);
+        LocalDateTime eventDate3 = LocalDateTime.of(2020, 11, 4, 11, 0);
+        LocalDateTime eventDate4 = LocalDateTime.of(2020, 10, 3, 11, 11);
+        LocalDateTime eventDate5 = LocalDateTime.of(2021, 11, 3, 11, 11);
 
+        manager.createEvent("1", eventDate1, eventDate1.plusHours(1), "event1");
+        manager.createEvent("2", eventDate2, eventDate2.plusHours(1), "event2");
+        manager.createEvent("1", eventDate3, eventDate3.plusHours(1), "event3");
+
+        assertFalse(manager.canBook("1", eventDate1, eventDate1.plusMinutes(10)));
+        assertFalse(manager.canBook("1", eventDate1, eventDate1.plusHours(10)));
+        assertTrue(manager.canBook("2", eventDate1, eventDate1.plusMinutes(10)));
+        assertFalse(manager.canBook("1", eventDate4, eventDate1.plusMinutes(120)));
+        assertTrue(manager.canBook("1", eventDate5, eventDate1.plusMinutes(120)));
+        assertFalse(manager.canBook("2", eventDate2.minusHours(1), eventDate2.plusMinutes(1)));
+        assertTrue(manager.canBook("2", eventDate2.minusHours(1), eventDate2.minusMinutes(1)));
     }
 
     @Test
@@ -65,6 +86,31 @@ public class EventManagerTest {
         assertFalse(manager.isScheduleFree(eventDate1));
         assertFalse(manager.isScheduleFree(eventDate4));
         assertTrue(manager.isScheduleFree(eventDate6));
+    }
+
+    @Test
+    public void testBooleans() {
+        EventManager manager = new EventManager();
+        LocalDateTime eventDate1 = LocalDateTime.of(2020, 11, 3, 11, 11);
+        LocalDateTime eventDate2 = LocalDateTime.of(2020, 12, 3, 10, 11);
+        LocalDateTime eventDate3 = LocalDateTime.of(2020, 11, 4, 11, 0);
+
+        manager.createEvent("1", eventDate1, eventDate1.plusHours(1), "event1");
+        manager.createEvent("2", eventDate2, eventDate2.plusHours(1), "event2");
+        manager.createEvent("3", eventDate3, eventDate3.plusHours(1), "event3");
+
+        assertTrue(manager.canAttend("event1"));
+        assertTrue(manager.canAttend("event2"));
+        assertTrue(manager.canAttend("event3"));
+
+        manager.addAttendee("event1", "A01");
+        manager.addAttendee("event1", "A02");
+
+        assertFalse(manager.canAttend("event1"));
+        assertTrue(manager.canAttend("event2"));
+
+        manager.removeAttendee("event1", "A01");
+        assertTrue(manager.canAttend("event1"));
     }
 
 }
