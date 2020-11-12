8f30725e8833f85215ca5c58cedc2ee978a7db89
alexpejovic <62117049+alexpejovic@users.noreply.github.com>
2020-11-12 13:09:29 -0500

    Implemented LogInUI, with some minor additional support to LoginController. Also commented out a line in Speaker controller causing compiling errors.

---
+++ phase1/src/Modules/UI/LogInUI.java
@@ -0,0 +1,42 @@
+package Modules.UI;
+
+import Modules.Controllers.LoginController;
+import java.util.Scanner;
+
+public class LogInUI {
+
+    private LoginController loginController;
+
+    private Scanner input = new Scanner(System.in);
+
+    /** Constructor for LogInUI
+     *
+     * @param loginController Controller which handles user inputs regarding logging-in
+     */
+    LogInUI(LoginController loginController) {
+        this.loginController = loginController;
+    }
+
+
+    /** Prompts user to log into their account within the console, and stores user information within loginController
+     *  after log-in is successful.
+     *
+     * @return True if log-in is successful, false otherwise
+     */
+    public boolean run(){
+
+        String username;
+        String password;
+
+        System.out.println("---Log In---\n");
+
+        do {
+            System.out.println("\nEnter Username");
+            username = input.nextLine();
+            System.out.println("\nEnter Password");
+            password = input.nextLine();
+        } while (loginController.validateUsernamePassword(username, password));
+
+        return loginController.logIn(username, password);
+    }
+}
