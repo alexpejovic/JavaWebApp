ea60872a17bae4a25e01c9cde095dd8ed8e70e1d
alexpejovic <62117049+alexpejovic@users.noreply.github.com>
2020-11-15 14:40:52 -0500

    Finalized changes in CancelEnrollmentUI

--- phase1/src/Modules/UI/CancelEnrollmentUI.java
+++ phase1/src/Modules/UI/CancelEnrollmentUI.java
@@ -6,17 +6,30 @@ import Modules.Presenters.EventPresenter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * UI for an attendee's cancellation of an enrollment into an event. run() method is used to execute this UI.
+ */
 public class CancelEnrollmentUI {
 
     private AttendeeController attendeeController;
     private EventPresenter eventPresenter;
 
+    /** Constructor for the UI.
+     *
+     * @param attendeeController Controller which handles user input
+     * @param eventPresenter Presenter which formats how the event list will be presenter
+     */
     CancelEnrollmentUI(AttendeeController attendeeController, EventPresenter eventPresenter){
         this.attendeeController = attendeeController;
         this.eventPresenter = eventPresenter;
     }
 
 
+    /** Displays to the user a list of events which they are currently attending. The user is given one chance to
+     * un-enroll in one of these events.
+     *
+     * @return True if cancellation successful, false otherwise
+     */
     public boolean run(){
 
         Scanner input = new Scanner(System.in);
@@ -25,7 +38,7 @@ public class CancelEnrollmentUI {
         ArrayList<String> eventNames = eventPresenter.getEventNames(attendeeController.getAttendingEvents());
         int max = eventStrings.size();
 
-        System.out.println("---Options to Cancel Enrollment");
+        System.out.println("---Options to Cancel Enrollment---");
 
         for(String event: eventStrings){
             System.out.println(event);
