a728c32b66eec05f201b03265ec7c11959c91ab4
sofija <kotaracsofija@gmail.com>
2020-11-16 01:34:18 -0500

    added comments to EventCreator

--- phase1/src/Modules/Controllers/EventCreator.java
+++ phase1/src/Modules/Controllers/EventCreator.java
@@ -11,10 +11,22 @@ import java.time.LocalDateTime;
 public class EventCreator {
     private EventManager eventManager;
 
+    /**
+     * Constructor for EventCreator
+     * @param eventManager the corresponding Manager needed to enter events into the system
+     */
     public EventCreator(EventManager eventManager) {
         this.eventManager = eventManager;
     }
 
+    /**
+     * Creates a new event with a unique ID and enters it into the system
+     * Given that the room is available at the specified time
+     * @param startTime the time the event starts
+     * @param endTime the time the event ends
+     * @param roomNumber the room number where the event will take place
+     * @return true if the event was successfully created and entered into the system, false if it wasn't
+     */
     public boolean createEvent(LocalDateTime startTime, LocalDateTime endTime, String roomNumber) {
         boolean eventCreated = true;
         try {
