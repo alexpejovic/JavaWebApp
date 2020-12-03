560be3a157a112a6eba11191f4907a29d884cc4e
Evan Rehbeli <evan.rehbeli@mail.utoronto.ca>
2020-12-03 16:21:57 -0500

    DBconnect gateway now returns a Connection for use in queries.

--- phase2/src/main/java/modules/gateways/DBConnect.java
+++ phase2/src/main/java/modules/gateways/DBConnect.java
@@ -6,16 +6,18 @@ import java.sql.SQLException;
 
 public class DBConnect {
 
-    public static void connect() throws ClassNotFoundException {
+    public static Connection connect() throws ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
 
         Connection conn = null;
 
         try {
             conn = DriverManager.getConnection("jdbc:sqlite:conference.db");
+            return conn;
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            return null;
         } finally {
             try {
                 if (conn != null) {
