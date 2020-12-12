8dbf533b13c5e563ecbc050ba5a0459c19ae5645
Khachig Ainteblian <ainteblian99@gmail.com>
2020-12-11 20:20:18 -0500

    merge loginscript

--- phase2/src/main/resources/web/scripts/loginscript.js
+++ phase2/src/main/resources/web/scripts/loginscript.js
@@ -1,10 +1,6 @@
-//var form = document.querySelector(".login-form");
-//var usr = document.querySelector("#username");
-//var pwd = document.querySelector("#password");
-
 const http = new XMLHttpRequest();
-url = "/getstatus";
-http.open("GET", url);
+url = "/getmodel";
+http.open("GET", url, true);
 http.send();
 
 http.onreadystatechange = function() {
@@ -27,40 +23,5 @@ function handle(response) {
             newText.id = "errorMessage"
             newText.innerText = response.statusMessage;
             form.appendChild(newText);
-    }
-}
-
-/*
-    Opens tab to access organizer-specific functions. NOTE: Still requires code to check if user is an organizer.
-*/
-function openTab(event, tabName) {
-    var i;
-    var content = document.getElementsByClassName("tabcontent");
-    var links = document.getElementsByClassName("tablinks");
-
-    for (i = 0; i < content.length; i++) {
-        content[i].style.display = "none";
-    }
-
-    for (i = 0; i < links.length; i++) {
-        links[i].className = links[i].className.replace(" active", "");
-    }
-
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
-}
-
-/* Resize the top navigation bar when the user scrolls down the page */
-window.onscroll = function() {
-    resizeNav();
-}
-
-function resizeNav() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("nav").style.padding = "30px 10px";
-        document.getElementById("nav-text").style.fontSize = "25px";
-    } else {
-        document.getElementById("nav").style.padding = "80px 10px";
-        document.getElementById("nav-text").style.fontSize = "35px";
     }
 }
