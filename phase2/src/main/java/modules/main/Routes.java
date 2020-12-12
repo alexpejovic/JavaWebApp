175b817b419780a381a1c947691b2171458f629a
Khachig Ainteblian <ainteblian99@gmail.com>
2020-12-11 20:33:02 -0500

    updated Routes

--- phase2/src/main/java/modules/main/Routes.java
+++ phase2/src/main/java/modules/main/Routes.java
@@ -2,6 +2,8 @@ package modules.main;
 
 import modules.controllers.ConfBuild;
 import modules.controllers.LoginController;
+import modules.controllers.OrganizerController;
+import modules.presenters.Model;
 import org.apache.log4j.BasicConfigurator;
 import org.json.simple.JSONObject;
 import spark.ModelAndView;
@@ -15,10 +17,13 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class Routes {
-    static ConfBuild confBuild = new ConfBuild();
-    static Map<String, Object> model = new HashMap<>();
+    static Map<String, Object> mdl = new HashMap<>();
     static String templatePath = "src/main/resources/web/endpoints/";
-    static Status status = new Status();
+    static Model model = new Model();
+    static ConfBuild confBuild = new ConfBuild(model);
+
+    static String currentUser;
+    static String userType;
 
     public static void main(String[] args) {
 
@@ -30,7 +35,7 @@ public class Routes {
             if (req.session().attribute("currentUser") == null) {
                 res.redirect("/login"); return "";
             }
-            return render(model, templatePath + "signin.html");
+            return render(mdl, templatePath + "signin.html");
         });
 
         get("/login", (req, res) -> {
@@ -40,13 +45,13 @@ public class Routes {
 
         get("/logout", (req, res) -> {
            req.session().removeAttribute("currentUser");
-           status.setStatus("logout");
-           status.clearMessage();
+           model.clear();
+           model.setStatus("logout");
            res.redirect("/home"); return "";
         });
 
-        get("/getstatus", (req, res) -> {
-            return status.toJSON();
+        get("/getmodel", (req, res) -> {
+            return model.toJSON();
         });
 
         post("/signin", Routes::handleLogin);
@@ -61,21 +66,28 @@ public class Routes {
         LoginController lc = confBuild.getLoginController();
         boolean login = lc.logIn(request.queryParams("user"), request.queryParams("pass"));
         if (login) {
-            String curUser = lc.getLoggedUser();
-            request.session().attribute("currentUser", curUser);
-            model.put("user", curUser);
+            currentUser = lc.getLoggedUser();
+            userType = lc.getUserType();
+            request.session().attribute("currentUser", currentUser);
+            mdl.put("user", currentUser);
+            updateModel();
             response.redirect("/home");
+            return "";
         }
         JSONObject stat = new JSONObject();
-        status.setStatus("error");
-        status.setMessage("Invalid username or password");
+        model.setStatus("error");
+        model.setStatusMessage("Invalid username or password");
         response.redirect("/home");
         return "";
     }
 
-//    private static Map<String, Object> fetchData(String userType, String userID) {
-//        if (userType.equals("organizer")) {
-//            OrganizerController controller = confBuild.getOrgController(userID);
-//        }
-//    }
+    private static void updateModel() {
+        System.out.println("fetching");
+        System.out.println(userType);
+        if (userType.equals("organizer")) {
+            System.out.println("condition holds");
+            OrganizerController controller = confBuild.getOrgController(currentUser);
+            controller.updateModel();
+        }
+    }
 }
